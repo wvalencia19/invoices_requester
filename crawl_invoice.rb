cf7e1971661ec7b8dbf71b3957f3a6f715407f45
Wilson Valencia <wvalencia@tappsi.co>
2018-06-25 11:56:58 -0500

    Initial project structure

---
+++ crawl_invoice.rb
@@ -0,0 +1,8 @@
+class CrawlInvoice
+  def initialize(id)
+  end
+
+  def get_total_invoices(init_date, fin_date)
+  end
+end
+
---
+++ customer.rb
@@ -0,0 +1,11 @@
+require_relative 'crawl_invoice'
+
+class Customer
+	def initialize(customer_id)
+		@customer_id = customer_id
+	end
+
+	def crawling_cutomer_total_invoices
+	end
+end
+
