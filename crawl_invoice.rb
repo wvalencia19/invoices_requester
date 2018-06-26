0d2dca7df064127e35f927d5193d01cfa908f8ec
Wilson Valencia <wvalencia@tappsi.co>
2018-06-26 17:03:53 -0500

    Add crawl invoice servive test

--- crawl_invoice.rb
+++ crawl_invoice.rb
@@ -1,8 +1,16 @@
+require 'httparty'
+
 class CrawlInvoice
+  include HTTParty
+  base_uri '34.209.24.195'
+
   def initialize(id)
+        @id = id
   end
 
   def get_total_invoices(init_date, fin_date)
+    options = { query: { id: @id, start: init_date, finish: init_date } }
+    self.class.get("/facturas", options)
   end
 end
 
