130906eac4f07b1fb7e3dff89fb39d93d72f0d9b
Wilson Valencia <wvalencia@tappsi.co>
2018-07-03 16:25:38 -0500

    Change on cral_invoice_spec to work with new signature

--- spec/crawl_invoice_spec.rb
+++ spec/crawl_invoice_spec.rb
@@ -3,8 +3,10 @@ require_relative '../crawl_invoice'
 RSpec.describe "crawl invoices service" do
   context "when service is online" do
     it "get 200 response code" do
-      crawl_invoice = CrawlInvoice.new("4e25ce61-e6e2-457a-89f7-116404990967")
-      response = crawl_invoice.get_total_invoices("2017-01-1", "2017-12-31")
+      crawl_invoice = CrawlInvoice.new
+      response = crawl_invoice.get_total_invoices("4e25ce61-e6e2-457a-89f7-116404990967",
+                                                  "2017-01-1",
+                                                  "2017-12-31")
       expect(response).to be_success
     end
   end
